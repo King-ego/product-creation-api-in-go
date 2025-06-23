c901472fbc519997f100f60c7823cf7cae23c216
Diego <diegosantos4957@gmail.com>
2025-06-23 08:19:30 -0300

    feat: change route /status for /

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	server := gin.Default()
 
-	server.GET("/status", func(ctx *gin.Context) {
+	server.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "ok",
 		})

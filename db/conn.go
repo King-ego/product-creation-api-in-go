da0e627e65ab4d6c18d4a82beb1cd705ae36d83d
Diego <diegosantos4957@gmail.com>
2025-06-24 20:02:12 -0300

    feat: connect to db

---
+++ db/conn.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
+func connectDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, error := sql.Open("postgres", psqlInfo)
+	if error != nil {
+		panic(error)
+	}
+
+	error = db.Ping()
+
+	if error != nil {
+		panic(error)
+
+	}
+
+	fmt.Println("connected to " + dbname)
+
+	return db, nil
+}

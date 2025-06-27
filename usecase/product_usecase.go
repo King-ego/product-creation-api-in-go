28d3620c361a9550a27fb4ea51f7a89dd2dc6241
Diego <diegosantos4957@gmail.com>
2025-06-26 23:18:06 -0300

    feat: iniciar usecase product

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -1,11 +1,18 @@
 package usecase
 
-import "api/model"
+import (
+	"api/model"
+	"api/repository"
+)
 
-type ProductUseCase struct{}
+type ProductUseCase struct {
+	repository repository.ProductRepository
+}
 
-func NewProductUseCase() ProductUseCase {
-	return ProductUseCase{}
+func NewProductUseCase(repo repository.ProductRepository) ProductUseCase {
+	return ProductUseCase{
+		repository: repo,
+	}
 }
 
 func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {

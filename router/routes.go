8aaf0d7dd015dbb0b33ae769cd53742ee2912b88
Diego <diegosantos4957@gmail.com>
2025-07-02 10:06:33 -0300

    feat: função que recebe todas as rotas

---
+++ router/routes.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"api/routes"
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Router struct {
+	router *gin.Engine
+	db     *sql.DB
+}
+
+func NewRouters(router *gin.Engine, db *sql.DB) Router {
+	return Router{
+		router: router,
+		db:     db,
+	}
+}
+
+func (ru *Router) Routers() {
+	productRouter := routes.NewProductRouter(ru.router, ru.db)
+	userRouter := routes.NewUserRouter(ru.router, ru.db)
+
+	productRouter.Routers()
+	userRouter.Routers()
+}

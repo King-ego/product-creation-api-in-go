6c992fe62e95b73ea6e3b645bcaceea1a2d1833f
Diego <diegosantos4957@gmail.com>
2025-07-04 17:49:13 -0300

    feat: declarar rota

--- router/routes.go
+++ router/routes.go
@@ -22,7 +22,9 @@ func NewRouters(router *gin.Engine, db *sql.DB) Router {
 func (ru *Router) Routers() {
 	productRouter := routes.NewProductRouter(ru.router, ru.db)
 	userRouter := routes.NewUserRouter(ru.router, ru.db)
+	CommentRouter := routes.NewCommentRouters(ru.router, ru.db)
 
 	productRouter.Routers()
 	userRouter.Routers()
+	CommentRouter.Routers()
 }
